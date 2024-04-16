13c48329a7220d235617e4389841e4b9441b8863
david-why <david_why@outlook.com>
2024-04-16 20:48:14 +0800

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,50 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
+import { fileURLToPath, URL } from 'node:url'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{css,js,svg,ico,html}'],
+        swDest: 'dist/sw.js'
+      },
+      manifest: {
+        name: 'AP History Review Tree',
+        short_name: 'AP Review Tree',
+        theme_color: '#001529',
+        background_color: '#ccc',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    }),
     Components({
       resolvers: [
         AntDesignVueResolver({
@@ -26,8 +62,8 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          apwh: ['@/assets/apwh.json'],
-          apeh: ['@/assets/apeh.json'],
+          'data-apwh': ['@/assets/apwh.json'],
+          'data-apeh': ['@/assets/apeh.json'],
           'antd-tree': ['ant-design-vue/es/tree'],
           antd: ['ant-design-vue']
         }
